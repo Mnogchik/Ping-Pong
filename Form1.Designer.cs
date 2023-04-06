8ce31e7d2821a0cf6193fa55fc024606a42a63db
Sergey <polozhencevsergei@mail.ru>
2023-04-06 16:36:10 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace test_c_sharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::test_c_sharp.Properties.Resources._1662044139_j_17;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(803, 523);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 482);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score: 0 : 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(201, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 45);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X игрок победил!";
+            this.label2.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(270, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сыграть еще раз";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 523);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ping Pong";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace test_c_sharp
+{
+    public partial class Form1 : Form
+    {
+        /*
+        мяч всегда летит в сторону того, кто забил последний шар
+        начальное направление и сторона мяча рандомные
+        со временем мяч начинает ускоряться до след. забитого шара
+        кнопка рестарта
+        
+        управление player 1 - A D
+                   player 2 - 4 6 (стрелки вообще не обрабатываются в WinForms)
+        */
+
+        // настройки игры
+        int platformsWidth = 120, platformsHeight = 10;
+        int ballRadius = 10;
+        int moveSpeed = 30;
+        int ballSpeed = 3;
+        uint winScore = 3;
+        ////////////////////
+
+        Random rand = new Random();
+
+        Point platformScale;
+        Point platform1, platform2, ball;
+        Rectangle platf1, platf2, rectBall;
+
+        int moveX, moveY;
+        uint blueScore = 0, redScore = 0;
+        float acceleration = 1f;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            platformScale = new Point(platformsWidth, platformsHeight);
+
+            platform1 = new Point(pictureBox1.Width / 2, pictureBox1.Height * 7 / 8);
+            platform2 = new Point(pictureBox1.Width / 2, pictureBox1.Height * 1 / 8);
+
+            int platform1LeftX = platform1.X - platformScale.X / 2,
+                platform1TopY = platform1.Y - platformScale.Y / 2;
+            platf1 = new Rectangle(platform1LeftX, platform1TopY, platformScale.X, platformScale.Y);
+
+            int platform2LeftX = platform2.X - platformScale.X / 2,
+                platform2TopY = platform2.Y - platformScale.Y / 2;
+            platf2 = new Rectangle(platform2LeftX, platform2TopY, platformScale.X, platformScale.Y);
+
+            moveX = rand.Next(0, 2) * 2 - 1; // -1 или 1
+            moveY = rand.Next(0, 2) * 2 - 1; // -1 или 1
+
+            ball = new Point(pictureBox1.Width / 2, pictureBox1.Height * (moveY == 1 ? 1 : 3) / 4);            
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            int platform1LeftX = platform1.X - platformScale.X / 2,
+                platform1TopY = platform1.Y - platformScale.Y / 2;
+            platf1 = new Rectangle(platform1LeftX, platform1TopY, platformScale.X, platformScale.Y);
+
+            e.Graphics.FillRectangle(Brushes.Red, platf1);
+
+            int platform2LeftX = platform2.X - platformScale.X / 2,
+                platform2TopY = platform2.Y - platformScale.Y / 2;
+            platf2 = new Rectangle(platform2LeftX, platform2TopY, platformScale.X, platformScale.Y);
+
+            e.Graphics.FillRectangle(Brushes.Blue, platf2);
+
+            rectBall = new Rectangle(ball.X - ballRadius, ball.Y - ballRadius, 2 * ballRadius, 2 * ballRadius);
+            e.Graphics.FillEllipse(Brushes.Yellow, rectBall);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            blueScore = redScore = 0;
+
+            button1.Visible = false;
+            label2.Visible = false;
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ball = new Point(
+                 ball.X + (int)(moveX * ballSpeed * acceleration),
+                 ball.Y + (int)(moveY * ballSpeed * acceleration));
+
+            acceleration += 0.0005f;
+
+            int ballLeftX = ball.X - ballRadius,
+                ballRightX = ball.X + ballRadius,
+                ballBottomY = ball.Y + ballRadius,
+                ballTopY = ball.Y - ballRadius;
+
+            if (ballInPlatform1(rectBall))
+                moveY = -1;
+            else if (ballInPlatform2(rectBall))
+                moveY = 1;
+
+            if (ballLeftX <= 1 || ballRightX >= pictureBox1.Width)
+                moveX = -moveX;
+
+            if (ballBottomY >= pictureBox1.Height)
+            {
+                blueScore++;
+                ball = new Point(pictureBox1.Width / 2, pictureBox1.Height * 3 / 4);
+                moveY = -1;
+                acceleration = 1f;
+            }
+            else if (ballTopY <= 0)
+            {
+                redScore++;
+                ball = new Point(pictureBox1.Width / 2, pictureBox1.Height * 1 / 4);
+                moveY = 1;
+                acceleration = 1f;
+            }
+
+            label1.Text = $"Score: {blueScore} : {redScore}";
+
+
+            if (blueScore == winScore || redScore == winScore)
+            {
+                label2.Visible = true;
+                button1.Visible = true;
+
+                if (blueScore == winScore)
+                    label2.Text = "Синий игрок победил!";
+                else
+                    label2.Text = "Красный игрок победил!";
+
+                timer1.Stop();
+            }
+
+            pictureBox1.Refresh();
+        }
+
+        private bool ballInPlatform1(Rectangle ball)
+        {
+            return dotInPlatform1(new Point(ball.Left, ball.Top)) || dotInPlatform1(new Point(ball.Left, ball.Bottom))
+                || dotInPlatform1(new Point(ball.Right, ball.Top)) || dotInPlatform1(new Point(ball.Right, ball.Bottom));
+        }
+
+        private bool ballInPlatform2(Rectangle ball)
+        {
+            return dotInPlatform2(new Point(ball.Left, ball.Top)) || dotInPlatform2(new Point(ball.Left, ball.Bottom))
+                || dotInPlatform2(new Point(ball.Right, ball.Top)) || dotInPlatform2(new Point(ball.Right, ball.Bottom));
+        }
+
+        private bool dotInPlatform1(Point point)
+        {
+            return point.X > platf1.Left && point.X < platf1.Right && point.Y > platf1.Top && point.Y < platf1.Bottom;
+        }
+
+        private bool dotInPlatform2(Point point)
+        {
+            return point.X > platf2.Left && point.X < platf2.Right && point.Y > platf2.Top && point.Y < platf2.Bottom;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'a')
+                platform1 = new Point(Clamp(platform1.X - moveSpeed,   0, pictureBox1.Width), platform1.Y );
+            else if (e.KeyChar == 'd')
+                platform1 = new Point(Clamp(platform1.X + moveSpeed,   0, pictureBox1.Width), platform1.Y);
+
+            if (e.KeyChar == '4')
+                platform2 = new Point(Clamp(platform2.X - moveSpeed,   0, pictureBox1.Width), platform2.Y);
+            else if (e.KeyChar == '6')
+                platform2 = new Point(Clamp(platform2.X + moveSpeed,   0, pictureBox1.Width), platform2.Y);
+        }
+
+
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            else 
+                return value;
+        }
+    }
+}
